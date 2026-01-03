cc312c404e1afcf41cc484778c6cab3edadaea20
alias-ktc <alenpeni.info@gmail.com>
2026-01-03 19:01:52 +1000

    Revert: Remove electrontRPC

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -1,15 +1,9 @@
 import { sha256sum } from "./nodeCrypto.js";
 import { versions } from "./versions.js";
 import { ipcRenderer } from "electron";
-import { exposeElectronTRPC } from "electron-trpc/main";
 
 function send(channel: string, message: string) {
   return ipcRenderer.invoke(channel, message);
 }
-
-// Expose tRPC
-process.once("loaded", async () => {
-  exposeElectronTRPC();
-});
 
 export { sha256sum, versions, send };
