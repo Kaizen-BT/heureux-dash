d5b7331e4728dcd2d296d71ba46381829c129ede
alias-ktc <alenpeni.info@gmail.com>
2026-01-04 16:22:13 +1000

    removed: unused proxy api

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -1,17 +1,9 @@
 import { sha256sum } from "./nodeCrypto.js";
 import { versions } from "./versions.js";
 import { ipcRenderer } from "electron";
-import { createAPIProxy } from "@app/ipc-bridge/client";
 
 function send(channel: string, message: string) {
   return ipcRenderer.invoke(channel, message);
 }
-
-const myAPI = {
-  getName: (name: string) => `${name}`,
-};
-
-const proxy = createAPIProxy({ api: myAPI });
-console.log(proxy.getName("Alen"));
 
 export { sha256sum, versions, send };
