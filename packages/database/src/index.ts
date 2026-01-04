9181aff5203a4ba00ba2b820893475bb189785ee
alias-ktc <alenpeni.info@gmail.com>
2026-01-04 20:41:41 +1000

    modified: cleaned up package exports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -8,7 +8,7 @@ import type { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import { drizzle } from "drizzle-orm/better-sqlite3";
-import * as schema from "./schema-barrel";
+import * as schema from "./schemas";
 
 interface SetupDatabaseProps {
   sqliteFilePath: string;
