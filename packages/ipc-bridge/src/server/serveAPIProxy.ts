c967d3d5914ae661ffda2782e20b4c689173d4c0
alias-ktc <alenpeni.info@gmail.com>
2026-01-05 15:06:45 +1000

    modified: ipc-bridge proxies

--- packages/ipc-bridge/src/server/serveAPIProxy.ts
+++ packages/ipc-bridge/src/server/serveAPIProxy.ts
@@ -4,7 +4,7 @@ import { APIProxyProps } from "../types";
 // Very primitive implementation of the ICP API Server...
 export function serveAPIProxy<T extends object>({ api }: APIProxyProps<T>) {
   Object.entries(api).forEach(([channel, handler]) => {
-    ipcMain.handle(channel, (_, args) => handler(...args));
+    ipcMain.handle(channel, (_, ...args) => handler(...args));
     console.log(`Registered: ${channel}`);
   });
 }
