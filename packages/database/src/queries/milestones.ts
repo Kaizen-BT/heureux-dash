30849379a69f86c9176f86e0a1bc1f8400504e4f
alias-ktc <alenpeni.info@gmail.com>
2026-01-05 14:53:16 +1000

    added: deprecation warnings for old queries

--- packages/database/src/queries/milestones.ts
+++ packages/database/src/queries/milestones.ts
@@ -1,5 +1,8 @@
 import type { BaseQueryProps } from "../types";
 
+/**
+ * @deprecated - use QueryDriver
+ */
 export async function getMilestones({ db }: BaseQueryProps) {
   return await db.query.milestones.findMany();
 }
--- packages/database/src/queries/tasks.ts
+++ packages/database/src/queries/tasks.ts
@@ -1,5 +1,8 @@
 import type { BaseQueryProps } from "../types";
 
+/**
+ * @deprecated - use QueryDriver
+ */
 export async function getTasks({ db }: BaseQueryProps) {
   return await db.query.tasks.findMany();
 }
