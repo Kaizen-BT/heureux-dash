88c87bc8b576906d57385df346cc6b6545139bd7
alias-ktc <alenpeni.info@gmail.com>
2026-01-07 12:32:34 +1000

    added: documentation for sidebar interfaces & components

--- packages/renderer/src/components/sidebars/base-sidebar.tsx
+++ packages/renderer/src/components/sidebars/base-sidebar.tsx
@@ -4,22 +4,27 @@ import {
   SidebarSeparator,
   Sidebar,
 } from "@/components/ui/sidebar";
-import type { ComponentProps } from "react";
+import type { ComponentProps, ReactElement } from "react";
 import { SidebarCalendar } from "../date-picker";
 
 /**
- * Generic Content Sidebar helper
+ * Helper for sidebars that render content into SidebarContent
+ * @see {@link https://ui.shadcn.com/docs/components/sidebar#structure}
  */
 export interface ContentSidebarProps<T> extends ComponentProps<typeof Sidebar> {
+  /**
+   * Generic array to be mapped and rendered into elements inside SidebarContent
+   */
   sidebarContent: T[];
 }
 
 /**
- * Temporary placehold sidebar
+ * Temporary placeholder sidebar
+ * @returns {ReactElement}
  */
 export function SidebarExample({
   ...props
-}: React.ComponentProps<typeof Sidebar>) {
+}: React.ComponentProps<typeof Sidebar>): ReactElement {
   return (
     <Sidebar {...props}>
       <SidebarHeader className="border-sidebar-border h-16 border-b" />
