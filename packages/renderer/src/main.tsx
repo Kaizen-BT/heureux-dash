66fe657b28c2a2444b678e0ef7cf7174cae6fbdd
alias-ktc <alenpeni.info@gmail.com>
2026-01-10 20:41:36 +1000

    modified: removed unuseed components and renamed overview route

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import OverviewPage, { ProjectLoader } from "./routes/overview.tsx";
+import HomePage, { ProjectLoader } from "./routes/home.tsx";
 import { createHashRouter } from "react-router";
 import { RouterProvider } from "react-router";
 import { RootLayout } from "./layouts/root-layout.tsx";
@@ -8,7 +8,7 @@ import { RootLayout } from "./layouts/root-layout.tsx";
 const router = createHashRouter([
   {
     Component: RootLayout,
-    children: [{ index: true, Component: OverviewPage, loader: ProjectLoader }],
+    children: [{ index: true, Component: HomePage, loader: ProjectLoader }],
   },
 ]);
 
--- packages/renderer/src/routes/home.tsx
+++ packages/renderer/src/routes/home.tsx
@@ -1,1 +1,17 @@
-export function HomePage() {}
+import { getProject } from "@app/preload";
+import { Quote } from "@/components/quotes";
+
+export async function ProjectLoader() {
+  const project = await getProject(1);
+  return project;
+}
+
+export function HomePage() {
+  return (
+    <>
+      <Quote quote="Ngl you gotta lock in twin" />
+    </>
+  );
+}
+
+export default HomePage;
