90ee692562d450fa76e5ed5bfdc9310c263c68d5
alias-ktc <alenpeni.info@gmail.com>
2026-01-11 17:47:44 +1000

    modified: dashboard page to look closer to the mockup. Optimizations req

--- packages/renderer/src/routes/dashboard.tsx
+++ packages/renderer/src/routes/dashboard.tsx
@@ -1,6 +1,7 @@
 import { PageGrid } from "@/components/page-grid";
 import { TypographyH1 } from "@/components/typography";
-import { Card } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Bell, ListTodo, PanelsLeftBottom, Route } from "lucide-react";
 
 export function DashboardPage() {
   return (
@@ -8,16 +9,44 @@ export function DashboardPage() {
       <TypographyH1>Dashboard</TypographyH1>
       <PageGrid>
         {/* Project Progression Card */}
-        <Card className="col-span-4" />
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle className="flex flex-row gap-2">
+              <PanelsLeftBottom className="w-5 h-5 self-center" />
+              <h2 className="text-xl">Project Progression</h2>
+            </CardTitle>
+          </CardHeader>
+        </Card>
 
         {/* Reminder Card */}
-        <Card className="col-span-4" />
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle className="flex flex-row gap-2">
+              <Bell className="w-5 h-5 self-center" />
+              <h2 className="text-xl">Project Progression</h2>
+            </CardTitle>
+          </CardHeader>
+        </Card>
 
         {/* Upcoming Tasks Card */}
-        <Card className="row-span-2 col-span-4" />
+        <Card className="col-span-4 row-span-2">
+          <CardHeader>
+            <CardTitle className="flex flex-row gap-2">
+              <ListTodo className="w-5 h-5 self-center" />
+              <h2 className="text-xl">Project Progression</h2>
+            </CardTitle>
+          </CardHeader>
+        </Card>
 
         {/* Timeline Card */}
-        <Card className="col-span-8" />
+        <Card className="col-span-8">
+          <CardHeader>
+            <CardTitle className="flex flex-row gap-2">
+              <Route className="w-5 h-5 self-center" />
+              <h2 className="text-xl">Project Progression</h2>
+            </CardTitle>
+          </CardHeader>
+        </Card>
       </PageGrid>
     </>
   );
