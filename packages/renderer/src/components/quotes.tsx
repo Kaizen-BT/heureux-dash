13dd975d7e224e4ef29b82472b37158cf1de3aff
alias-ktc <alenpeni.info@gmail.com>
2026-01-11 15:25:48 +1000

    added: docs for Quote component

--- packages/renderer/src/components/quotes.tsx
+++ packages/renderer/src/components/quotes.tsx
@@ -1,11 +1,21 @@
 import { TypographyH1 } from "@/components/typography";
+import type { ReactElement } from "react";
 
 interface QuoteProps {
   quote: string;
   author?: string;
 }
 
-export function Quote({ quote, author = "Anonymous" }: QuoteProps) {
+/**
+ * Utility component to render a quote that will likely be fetched
+ * from an API once per day
+ *
+ * @returns {ReactElement}
+ */
+export function Quote({
+  quote,
+  author = "Anonymous",
+}: QuoteProps): ReactElement {
   return (
     <blockquote className="flex w-fit flex-col">
       <TypographyH1>{`" ${quote} "`}</TypographyH1>
