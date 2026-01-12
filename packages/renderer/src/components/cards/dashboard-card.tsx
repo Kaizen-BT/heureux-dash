ebf6252f7b3e469a124031552acc1eea23e39f15
alias-ktc <alenpeni.info@gmail.com>
2026-01-12 16:33:36 +1000

    modified: added autocompletion for cva and dashboard-card component

---
+++ packages/renderer/src/components/cards/dashboard-card.tsx
@@ -0,0 +1,4 @@
+import type { ComponentProps, ReactElement } from "react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
