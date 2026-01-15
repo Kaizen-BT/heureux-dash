6f2db2d413295d198ef8900b10a8c69d5b030a34
alias-ktc <alenpeni.info@gmail.com>
2026-01-15 18:56:35 +1000

    modified: reminder card in accordance with UI mockup

--- packages/renderer/src/components/cards/reminder-card.tsx
+++ packages/renderer/src/components/cards/reminder-card.tsx
@@ -1,6 +1,22 @@
 import { ListTodo } from "lucide-react";
-import { DashboardCard } from "@/components/cards/dashboard-card";
+import {
+  DashboardCard,
+  DashboardCardContent,
+} from "@/components/cards/dashboard-card";
+import { TypographyH1 } from "../typography";
 
 export function ReminderCard() {
-  return <DashboardCard title="Reminders" Icon={ListTodo} />;
+  return (
+    <DashboardCard title="Reminders" Icon={ListTodo}>
+      <DashboardCardContent className="w-full h-full flex flex-col">
+        <div className="my-auto">
+          <TypographyH1 className="w-1/2">Code Review</TypographyH1>
+        </div>
+        <div>
+          <p>Scheduled For:</p>
+          {/* Datepicker goes here */}
+        </div>
+      </DashboardCardContent>
+    </DashboardCard>
+  );
 }
